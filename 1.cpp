27cf0cf569f916eaf5bbbb9357c17195bd55f062
unknown <1628553830@qq.com>
2020-07-30 18:39:59 +0800

    summer

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+    string s,a;
+    int num;
+    cin>>s>>num;
+    for(int i=0;i<num;i++)
+    for(int j=i;j<num;j++)
+    {
+        
+    }
+    
+}
