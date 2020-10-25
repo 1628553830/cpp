65950421dce07475ed85a981ea8a5d991cf9a925
1628553830 <1628553830@qq.com>
2020-10-25 12:53:48 +0800

    acm

--- HDU/7.Tick and Tick.cpp
+++ HDU/7.Tick and Tick.cpp
@@ -2,33 +2,53 @@
 using namespace std;
 int main()
 {
-    int n,q=0;
-    double miao=0,fen,shi,t1,t2,t3,t4,ans=0;
+    int n,s1=0,s2=0,s3=0,s4=0;
+    double ms[2000],fs[30],fm[2000],h1[2000],ans=0;
     cin>>n;
     while(n<=120&&n>=0)
     {
-        //解多次方程
-        while(720/11*(n+q*360)<259200)//用度数来解方程
+        for(int i=0;(n+360*i)*720.0/719<259200;i++)
         {
-           t1=720/11*(n+q*360);q++;
-           t2=720/11*(q*360-n);//t1,t2用来确定分针和时针开心的边界,t3,t4用来确定秒针分别与时针和分针的开心边界
-           //用for循环寻找在t1-t2该区间的miao的范围
-           t3=t4=0;
-           
-
-           for(int i=0;t3<259200;i++)
-           {
-               t3=(n+(i*360))*720/719;
-               if(t1<t3&&t3<t2)break;
-           }
-           for(int i=0;t4<259200;i++)
-           {
-               t4=(360*(i+1)-n)*720/719;
-           }
-           
-            
+            ms[s1++]=(n+360*i)*720.0/719;
+            ms[s1++]=((i+1)*360-n)*720.0/719;
         }
-        
+
+        for(int i=0;(n+360*i)*720.0/11<259200;i++)
+        {
+            fs[s2++]=(n+360*i)*720.0/11;
+            fs[s2++]=((i+1)*360-n)*720.0/11;
+        }
+
+        for(int i=0;(n+360*i)*60.0/59<259200;i++)
+        {
+            fm[s3++]=(n+360*i)*60.0/59;
+            fm[s3++]=((i+1)*360-n)*60.0/59;
+        }
+        //合并fs和fm,并入h1之中
+        for(int i=0;i<s2;i+=2)
+          for(int j=0;j<s3&&fm[j]<fs[i+1];j+=2)
+          {
+              if(fm[j+1]>fs[i])
+              {
+                  h1[s4++]=max(fm[j],fs[i]);
+                  h1[s4++]=min(fm[j+1],fs[i+1]);
+              }
+          }
+        //合并h1和ms,并入fm
+        s3=0;
+        for(int i=0;i<s4;i+=2)
+        for(int j=0;j<s1&&ms[j]<h1[i+1];j+=2)
+        {
+            if(ms[j+1]>h1[i])
+            {
+                fm[s3++]=max(ms[j],h1[i]);
+                fm[s3++]=min(ms[j+1],h1[i+1]);
+            }
+        }
+        for(int i=0;i<s3;i+=2)ans+=fm[i+1]-fm[i];
+        printf("%.3f\n",ans/259200*100);
+        s1=s2=s3=s4=ans=0;
+        cin>>n;
     }
     cin.get();
     cin.get();
--- complete/B(p).cpp
+++ complete/B(p).cpp
@@ -3,7 +3,7 @@ int main()
 {
    long n;int ans=1;
    scanf("%ld",&n);
-   for(int i=2;i<n;)
+   for(int i=2;i<=sqrt(n);)
    {
      if(n%i==0){ans++;n/=i;}
      else i++;
