9e5e9db4656828e454026a4573e30dd5ecad25d3
1628553830 <1628553830@qq.com>
2020-12-08 15:47:14 +0800

    2048

---
+++ gamedesign/2048/main.cpp
@@ -0,0 +1,42 @@
+#include<conio.h>
+#include<graphics.h>
+#include<Windows.h>
+#include<stdlib.h>
+#include<time.h>
+#include"start.cpp"
+#define High 480
+#define Width 640
+
+int status[4][4];
+int score;
+
+
+
+
+void setup()
+{
+
+}
+void updateWithInput()
+{
+
+}
+void updateWithoutInput()
+{
+
+}
+void show()
+{
+
+}
+int main()
+{
+    setup();
+    startui();
+    while(1)
+    {
+        show();
+        updateWithInput();
+        updateWithoutInput();
+    }
+}
---
+++ gamedesign/2048/start.cpp
@@ -0,0 +1,132 @@
+#include<conio.h>
+#include<graphics.h>
+#include<Windows.h>
+#include<stdlib.h>
+#include<time.h>
+#include<iostream>
+using namespace std;
+string str;
+void Wchar_tToString(std::string& szDst, wchar_t*wchar)
+{
+wchar_t * wText = wchar;
+DWORD dwNum = WideCharToMultiByte(CP_OEMCP,NULL,wText,-1,NULL,0,NULL,FALSE);//WideCharToMultiByte的运用
+char *psText;  // psText为char*的临时数组，作为赋值给std::string的中间变量
+psText = new char[dwNum];
+WideCharToMultiByte (CP_OEMCP,NULL,wText,-1,psText,dwNum,NULL,FALSE);//WideCharToMultiByte的再次运用
+szDst = psText;// std::string赋值
+delete []psText;// psText的清除
+}
+
+void startui()
+{
+   //初始化背景
+
+   initgraph(640,480);
+   setbkcolor(GREEN);
+   setbkmode(TRANSPARENT);
+   cleardevice();
+   Wchar_tToString(str,L"2048");
+   SetWindowText(GetHWnd(),(LPCTSTR)str.c_str());
+   //初始化结构体
+   LOGFONT f;
+   RECT r{0,0,640,480};
+   settextstyle(0,0,_T("Consolas"));
+   gettextstyle(&f);
+   f.lfWeight=1000;
+   f.lfWidth=0;
+   f.lfQuality=ANTIALIASED_QUALITY;
+   
+   //输出黑字2
+   settextcolor(BLACK);
+   f.lfHeight=200;
+   _tcscpy(f.lfFaceName,_T("黑体"));
+   settextstyle(&f);
+   drawtext(_T("2"),&r,DT_CENTER|DT_VCENTER|DT_SINGLELINE);
+   Sleep(1000);
+   cleardevice();
+   //输出白字20
+   settextcolor(WHITE);
+   f.lfHeight=200;
+   _tcscpy(f.lfFaceName,_T("宋体"));
+   settextstyle(&f);
+   drawtext(_T("20"),&r,DT_CENTER|DT_VCENTER|DT_SINGLELINE);
+   Sleep(1000);
+   cleardevice();
+   //输出蓝字204
+   settextcolor(BLUE);
+   f.lfHeight=200;
+   _tcscpy(f.lfFaceName,_T("楷体"));
+   settextstyle(&f);
+   drawtext(_T("204"),&r,DT_CENTER|DT_VCENTER|DT_SINGLELINE);
+   Sleep(1000);
+   cleardevice();
+   //输出阴影部分
+   settextcolor(BLACK);
+   f.lfHeight=250;
+   _tcscpy(f.lfFaceName,_T("Consolas"));
+   settextstyle(&f);
+   outtextxy(95,120,_T("2048"));
+   //输出红字2048
+   settextcolor(RED);
+   drawtext(_T("2048"),&r,DT_CENTER|DT_VCENTER|DT_SINGLELINE);
+   Sleep(1000);
+
+   //输出Power by Group 7510
+   wchar_t s[23]=L"▂Powered by Group 7510";
+   wchar_t current[24];
+   current[22]='\0';
+   current[23]='\0';
+   settextcolor(BLACK);
+   f.lfHeight=50;
+   settextstyle(&f);
+   for(int i=0;i<22;i++)
+   {
+      bool flag=true;
+      for(int j=0;j<22;j++)
+      {
+          if(flag)
+          {
+              current[j]=s[j];
+              if(s[j]==L'▂')
+              {wchar_t temp;temp=s[j+1];s[j+1]=s[j];s[j]=temp;flag=false;}
+          }
+          else current[j]=L' ';
+      }
+      Wchar_tToString(str,current);
+      drawtext((LPCTSTR)str.c_str(),&r,DT_CENTER|DT_BOTTOM|DT_SINGLELINE);
+      Sleep(200);
+      settextcolor(GREEN);
+      drawtext((LPCTSTR)str.c_str(),&r,DT_CENTER|DT_BOTTOM|DT_SINGLELINE);
+      settextcolor(BLACK);
+   }
+   while(!kbhit())
+   {
+       BeginBatchDraw();
+       static bool flag=true;
+       if(flag)
+       {
+           current[21]=' ';
+           current[22]=' ';
+           Wchar_tToString(str,current);
+           outtextxy(60,430,(LPCTSTR)str.c_str());
+           FlushBatchDraw();
+           Sleep(500);
+           flag=!flag;
+       }
+       else
+       {
+           current[21]=L'▂';
+           current[22]='\0';
+           Wchar_tToString(str,current);
+           outtextxy(60,430,(LPCTSTR)str.c_str());
+           FlushBatchDraw();
+           Sleep(500);
+           settextcolor(GREEN);
+           outtextxy(60,430,(LPCTSTR)str.c_str());
+           settextcolor(BLACK);
+           flag=!flag;
+       }
+   }
+   _getch();
+   closegraph();
+}
